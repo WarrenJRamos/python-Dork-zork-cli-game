973836e8afee5279d52846ee2ca6863bb047f671
rrosebrook <31425480+rrosebrook@users.noreply.github.com>
2019-06-03 12:03:24 -0600

    added name

--- dork/__init__.py
+++ dork/__init__.py
@@ -4,6 +4,6 @@
 
 __version__ = '0.1.0'
 __author__ = ", ".join([
-    "Luke Smith", "Andres Restrepo", "Jacob Netwal", "Rachel Rosebrook"
+    "Luke Smith", "Andres Restrepo", "Jacob Netwal", "Rachel Rosebrook", "Warren J Ramos"
 ])
 
