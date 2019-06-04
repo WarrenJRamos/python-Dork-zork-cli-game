d69241617c3d8f773546f2ff45cbc3eb0c5c22b5
rrosebrook <31425480+rrosebrook@users.noreply.github.com>
2019-06-03 20:13:01 -0600

    Allow user input on hours worked

--- team notes/rachel rosebrook notes/HoursWorked.R
+++ team notes/rachel rosebrook notes/HoursWorked.R
@@ -1,12 +1,26 @@
-rachel <- 8
-warren <- 10
-jacob <- 12
-andres <- 16
+#rachel <- 8
+#warren <- 10
+#jacob <- 12
+#andres <- 16
 
-group <- c(andres, jacob, rachel, warren)
+group <- NULL
+hours <- NULL
+#group <- c(andres, jacob, rachel, warren)
 names <- c("Andres", "Jacob", "Rachel", "Warren")
+i <- 1
 
-sprint <- "Sprint 2"
+while(i < length(names) + 1) {
+  hours <- readline(paste("How many hours did ", names[i], " work?  " ))
+  group <- c(group, hours)  
+  i <- i + 1
+}
+group <- as.numeric(group)
+
+
+sprintNum <- readline(prompt = "Which sprint is this?       ")
+
+#sprint <- "Sprint 2"
+sprint <- cat("Sprint", sprintNum)
 
 pdf(paste(sprint, ".pdf", sep = ""))
 barplot(group, names.arg = names, 
