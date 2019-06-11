9ba656d4638dcf9fc35bcafef06a20ed008a7625
WarrenJRamos <wramos1@msudenver.edu>
2019-06-11 10:49:19 -0600

    Fixed pylint problems in this python file

--- team notes/warren joseph ramos notes/simple_graph.py
+++ team notes/warren joseph ramos notes/simple_graph.py
@@ -1,3 +1,5 @@
+''' Creates a simple graph '''
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -9,7 +11,7 @@ G.add_node("Room 5")
 G.add_edge("Room 2", "Room 5")
 G.add_edge("Room 4", "Room 1")
 
-G.add_edges_from([("Room 2", "Room 5"), ("Room 1", "Room 3"), ("Room 1","Room 2")])
+G.add_edges_from([("Room 2", "Room 5"), ("Room 1", "Room 3"), ("Room 1", "Room 2")])
 G.add_edges_from([("Room 6", "Room 4"), ("Room 5", "Room 6")])
 
 print(nx.info(G))
