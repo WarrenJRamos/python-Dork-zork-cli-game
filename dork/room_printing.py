f4cbe3d6eb522574f89c0663ebfe90786753b5f0
WarrenJRamos <wramos1@msudenver.edu>
2019-06-16 21:26:37 -0600

    Added more print statements

--- dork/room_printing.py
+++ dork/room_printing.py
@@ -1,4 +1,4 @@
-﻿
+﻿"""Game responses to user commands"""
 # pylint: disable=R0904
 # The print methods that have "change this later" means that we are not
 # using them for the current sprint. The disable above allows us to go over
@@ -19,31 +19,138 @@ class Room1Printing():
             if direction == "east":
                 print("There is a table in the room with a donut on top.")
             if direction == "west":
-                print("There is a beautiful garden with the roadrunner's nest right \nin the center of the garden. The nest has eggs that look about \nready to hatch but no roadrunner parent to be seen.")
+                print("There is a beautiful garden with the roadrunner's " +
+                      "nest right \nin the center of the garden. The nest " +
+                      "has eggs that look about \nready to hatch but no " +
+                      "roadrunner parent to be seen.")
 
-    def print_look(direction):
+        if room == "room 2":
+            if direction == "north":
+                print("You are in the dean’s office. The dean is irritated " +
+                      "that you are \nin the room. You have 1 minute to make " +
+                      "him happy; otherwise, campus \nsecurity will arrive " +
+                      "and take you away.")
+            if direction == "south":
+                print("You fall to your death.")
+            if direction == "east":
+                print("You are unable to go East.")
+            if direction == "west":
+                print("You are inside the Student Success Building. There " +
+                      "is a locked cage \nwith a roadrunner inside. It looks " +
+                      "starving and nervous. \nThere is a piece of paper " +
+                      "next to the cage.")
+
+        if room == "room 3":
+            if direction == "north":
+                print("You are in the dean’s office. The dean is irritated" +
+                      " that you are in \nthe room. You have 1 minute to make " +
+                      "him happy; otherwise, \ncampus security will arrive " +
+                      "and take you away.")
+            if direction == "south":
+                print("You are unable to go South.")
+            if direction == "east":
+                print("You are inside the Student Success Building. There " +
+                      "is a locked cage \nwith a roadrunner inside. It looks " +
+                      "starving and nervous. \nThere is a piece of paper " +
+                      "next to the cage.")
+            if direction == "west":
+                print("You are unable to go West.")
+
+        if room == "room 4":
+            if direction == "north":
+                # Ask teammates about whether or not to include an extra parameter for this method
+                print("The blue door is unlocked, and you enter a room with " +
+                      "a gold key.")
+                print("The dean is blocking the door and does not let you " +
+                      "through. You cannot enter this room.")
+            if direction == "south":
+                print("You are inside the Student Success Building. There " +
+                      "is a locked cage \nwith a roadrunner inside. It looks " +
+                      "starving and nervous. \nThere is a piece of paper " +
+                      "next to the cage.")
+            if direction == "east":
+                print("There is a table in the room with a donut on top.")
+            if direction == "west":
+                print("There is a beautiful garden with the roadrunner's " +
+                      "nest right \nin the center of the garden. The nest " +
+                      "has eggs that look about \nready to hatch but no " +
+                      "roadrunner parent to be seen.")
+
+        if room == "room 5":
+            if direction == "north":
+                print("You are unable to go North.")
+            if direction == "south":
+                print("You are in the dean’s office. The dean is irritated" +
+                      " that you are in \nthe room. You have 1 minute to make " +
+                      "him happy; otherwise, \ncampus security will arrive " +
+                      "and take you away.")
+            if direction == "east":
+                print("You are unable to go East.")
+            if direction == "west":
+                print("You are unable to go West.")
+
+    def print_look(room, direction):
         """
         Prints a statement after user looks a certain direction
         """
-        if direction == "north":
-            print("There is a door with a sign that says DANGER.")
-        if direction == "south":
-            print("There is bag of bird food with all sorts of insects " +
-                  "that roadrunners love to eat.")
-        if direction == "east":
-            print("To the East, there is a sign that says Lounge.")
-        if direction == "west":
-            print("To the West, there is a sign that says Roadrunner's Nest")
+        if room == "room 1":
+            if direction == "north":
+                print("There is a door with a sign that says DANGER.")
+            if direction == "south":
+                print("There is bag of bird food with all sorts of insects " +
+                      "that roadrunners love to eat.")
+            if direction == "east":
+                print("To the East, there is a sign that says Lounge.")
+            if direction == "west":
+                print("To the West, there is a sign that says Roadrunner's Nest")
 
-    @classmethod
-    def print_score(cls, score):
+        if room == "room 2":
+            if direction == "north":
+                print("There is a sign that says DANGER!!")
+            if direction == "south":
+                print("There is a 1000 ft cliff with spikes.")
+            if direction == "east":
+                print("There is an empty wall.")
+            if direction == "west":
+                print("There is a sign that says Student Success Building.")
+
+        if room == "room 3":
+            if direction == "north":
+                print("There is a sign that says DANGER!!")
+            if direction == "south":
+                print("There is an empty wall.")
+            if direction == "east":
+                print("There is a sign that says Student Success Building.")
+            if direction == "west":
+                print("There is an empty wall.")
+
+        if room == "room 4":
+            if direction == "north":
+                print("There is blue door that is being guarded by the dean.")
+            if direction == "south":
+                print("There is a sign that says Student Success Building.")
+            if direction == "east":
+                print("To the East, there is a sign that says Lounge.")
+            if direction == "west":
+                print("To the West, there is a sign that says Roadrunner’s Nest.")
+
+        if room == "room 5":
+            if direction == "north":
+                print("There is an empty wall.")
+            if direction == "south":
+                print("There is a blue door")
+            if direction == "east":
+                print("There is an empty wall.")
+            if direction == "west":
+                print("There is an empty wall.")
+
+    def print_score(score):
         """
         Prints out the score
         """
         print("You have a score of " + str(score))
 
-    @classmethod
-    def print_diagnostic(cls, health_score):
+    def print_diagnostic(health_score):
         """
         Prints out the health data of the player
         """
@@ -64,15 +171,13 @@ class Room1Printing():
         else:
             print(item + "was not found")
 
-    @classmethod
-    def print_drop_item(cls):
+    def print_drop_item(self):
         """
         Print the item being dropped
         """
         print("CHANGE THIS FOR FUTURE SPRINT")
 
-    @classmethod
-    def print_open_item(cls, item):
+    def print_open_item(item):
         """
         Print what happens after player open the item.
         """
@@ -81,15 +186,13 @@ class Room1Printing():
               "The bird is added to your inventory.")
         # Call the add_inventory() method here
 
-    @classmethod
-    def print_move_moveable(cls):
+    def print_move_moveable(self):
         """
         Prints what happens after the item is moved
         """
         print("CHANGE THIS LATER")
 
-    @classmethod
-    def print_attack(cls, creature):
+    def print_attack(creature):
         """
         Prints out what happens after the player attacks a creature
         """
@@ -151,9 +254,9 @@ class Room1Printing():
         if food is None:
             print("There is nothing to feed the bird with.")
 
-    @classmethod
-    def print_give_item(cls):
+    def print_give_item(self):
         """
         Print what happens after player gives the item
         """
         print("CHANGE THIS LATER")
+        
