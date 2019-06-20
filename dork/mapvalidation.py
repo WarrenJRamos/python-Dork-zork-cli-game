743ec5873531dab62be7f02a12e96aa80d41ac08
Andres Restrepo <arestre1@msudenver.edu>
2019-06-19 23:58:49 -0600

    Formatting Classes for pylint

--- dork/mapvalidation.py
+++ dork/mapvalidation.py
@@ -1,108 +1,88 @@
 """
 A class that validates a maze coming from a .yml/.yaml file
 """
-from dork.mazeroom import MazeRoom
-
-class ValidMaze():
+class ValidMaze:
     """
     A valid maze
     """
-    actual_maze = []
-    room_names = []
-    room_cardinals = []
-    valid_cardinals = ['north', 'east', 'south', 'west']
-    
-    def load_rooms(self, maze):
+    @classmethod
+    def load_rooms(cls, maze):
         """
         Loading the room names
         """
         for room_name in maze:
-            ValidMaze.room_names = list(maze[room_name].keys())
-    
-    def load_cardinals(self, maze):
+            room_names = list(maze[room_name].keys())
+        return room_names
+    @classmethod
+    def load_cardinals(cls, maze):
         """
         Loading the room cardinals
         """
         for room_name in maze:
-            ValidMaze.room_cardinals = list(maze[room_name].values())
-
-    def check_rooms(self):
+            room_cardinals = list(maze[room_name].values())
+        return room_cardinals
+    @classmethod
+    def check_rooms(cls, room_names):
         """
         Checking room names for valid names
         """
-        return None in ValidMaze.room_names
-
-    def check_cardinals(self):
+        return None in room_names
+    @classmethod
+    def check_cardinals(cls, room_cardinals):
         """
         Checking for valid cardinals in each room
         """
+        valid_cardinals = ['north', 'east', 'south', 'west']
         invalid_cardinal = False
 
-        for i in range(len(ValidMaze.room_cardinals)):
-            if  list(ValidMaze.room_cardinals[i].keys()) != ValidMaze.valid_cardinals:
+        for i in enumerate(len(room_cardinals)):
+            if  list(room_cardinals[i].keys()) != valid_cardinals:
                 invalid_cardinal = True
                 break
-            else:
-                continue
         return invalid_cardinal
-    def check_connections(self):
+    @classmethod
+    def check_connections(cls, room_names, room_cardinals):
         """
         Checking for cardinals to be pointing to unique and valid directions
         """
         dual_pointer = False
         invalid_direction = False
         isolated_room = False
-        for i in range(len(ValidMaze.room_cardinals)):
+        for i in enumerate(len(room_cardinals)):
 
-            adjacent_rooms = list(ValidMaze.room_cardinals[i].values())
+            adjacent_rooms = list(room_cardinals[i].values())
             adjacent_rooms [:] = (room for room in adjacent_rooms if room is not None)
             unique_rooms = set(adjacent_rooms)
             if len(adjacent_rooms) != len(unique_rooms):
                 dual_pointer = True
                 break
-            elif not set(adjacent_rooms).issubset(set(ValidMaze.room_names)):
+            elif not set(adjacent_rooms).issubset(set(room_names)):
                 invalid_direction = True
                 break
             elif unique_rooms.issubset({None}):
                 isolated_room = True
                 break
-            else:
-                continue
-
         return dual_pointer or invalid_direction or isolated_room
-
-    def maze_assembly(self):
-        """
-        Appending rooms to the maze
-        """
-
-        for i in range(len(ValidMaze.room_names)):
-            room = MazeRoom(ValidMaze.room_names[i],
-                            ValidMaze.room_cardinals[i]['north'],
-                            ValidMaze.room_cardinals[i]['east'],
-                            ValidMaze.room_cardinals[i]['south'],
-                            ValidMaze.room_cardinals[i]['west'])
-            ValidMaze.actual_maze.append(room)
-
-    def print_actual_maze(self):
+    @classmethod
+    def print_actual_maze(cls, room_names, room_cardinals):
         """
         Printing the maze
         """
-
         print('\n')
         print('The rooms are: \n')
 
-        for i in range(len(ValidMaze.actual_maze)):
-            print('>' + ValidMaze.actual_maze[i].name)
+        for i in enumerate(room_names):
+            print('>' + room_names[i])
         print('\n')
-        for i in range(len(ValidMaze.actual_maze)):
-            print(ValidMaze.actual_maze[i].name + ' is adjacent to: ')
+        for i in enumerate(room_names):
+            print(room_names[i] + ' is adjacent to: ')
             print('>North:')
-            print(ValidMaze.actual_maze[i].north)
+            print(room_cardinals[i]['north'])
             print('>East:')
-            print(ValidMaze.actual_maze[i].east)
+            print(room_cardinals[i]['east'])
             print('>South:')
-            print(ValidMaze.actual_maze[i].south)
+            print(room_cardinals[i]['west'])
             print('>West:')
-            print(ValidMaze.actual_maze[i].west)
+            print(room_cardinals[i]['south'])
             print('\n \n')
+    
--- dork/yamlreader.py
+++ dork/yamlreader.py
@@ -8,37 +8,28 @@ class YamlReader:
     """
     A .yml/.ymal reader
     """
-
-    def valid_path_file(self):
+    @classmethod
+    def user_input(cls):
+        """
+        user inputing file path
+        """
+        return input('Please type a valid file path for your .yml or .yaml file:\n')
+    @classmethod
+    def valid_file_path(cls, file_path):
         """
         checking for valid file path and valid extension
         """
-        validated = True
-
-        while validated:
-
-            file_path = input('Please type a valid file path for your .yml or .yaml file:\n')
-            valid_path = os.path.isfile(file_path)
-            valid_extension = YamlReader.valid_yml_file(self, file_path)
-
-            if not valid_path:
-                print(file_path + " is not a valid directory.")
-            elif not valid_extension:
-                print(file_path + " is not a .yml or .yaml file.")
-            else:
-                validated = False
-            data = YamlReader.yaml_loader(self, file_path)
-        return data
-
-    def valid_yml_file(self, file_path):
+        return os.path.isfile(file_path)
+    @classmethod
+    def valid_extension(cls, file_path):
         """
         validating .yml or .ymal extension
         """
         file_extension = os.path.splitext(file_path)
         valid_extension = file_extension[1] == '.yml' or file_extension[1] == '.yaml'
         return valid_extension
-
-    def yaml_loader(self, file_path):
+    @classmethod
+    def yaml_loader(cls, file_path):
         """
         reading the content of the .yml or .ymal file
         """
