4adf10ae95e4b959744c597cb3795ce68ce76bd4
Jacob Netwal <jnetwal@msudenver.edu>
2019-06-19 21:05:48 -0600

    .

--- tests/test_dork_cli.py
+++ tests/test_dork_cli.py
@@ -12,16 +12,16 @@ def test_cli_exists(run):
     """
     assert "main" in vars(dork.cli), "Dork.cli should define a main method"
     assert isinstance(dork.cli.main, FunctionType)
-  #  try:
-   #     run(dork.cli.main)
-  #  except:  # noqa: E722
-  #      raise AssertionError("cannot run 'dork' command")
+    try:
+        run(dork.cli.main)
+    except:  # noqa: E722
+        raise AssertionError("cannot run 'dork' command")
 
 
 def test_cli_help(run):
     """CLI's help command should return helpful information
     """
-  #  out, err = run(dork.cli.main, "-h")
- #   out, err = run(dork.cli.main(mock_cli), "-h")
+    out, err = run(dork.cli.main, "-h")
+    out, err = run(dork.cli.main(mock_cli), "-h")
     assert "usage: " in out, \
         "Failed to run the cli.main method: {err}".format(err=err)
