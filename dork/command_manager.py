f9c92b2701516d72f16458cca2d95cfb2fa78980
Jacob Netwal <jnetwal@msudenver.edu>
2019-06-19 18:52:26 -0600

    snakestyle

--- dork/command_manager.py
+++ dork/command_manager.py
@@ -1,38 +1,33 @@
 """This class acts as the controller for the game"""
 from dork.room_printing import Room1Printing
-#INPUT_LINE = ""
-#INPUT_HELPER = ""
-#GAME_OVER = False
-#PLAYER_INVENTORY = [""]
+#input_line = ""
+#input_helper = ""
+#game_over = False
+#player_inventory = [""]
 class CommandManager:
     """cli controller"""
 
     def __init__(self):
-        self.GAME_OVER = False
-        self.INPUT_LINE = ""
-        self.INPUT_HELPER = ""
-        self.PLAYER_INVENTORY = [""]
-
-    def start(self):
-        """
-        Initialize Starting Variables
-        """
-        print("Welcome to Dork!")
+        self.game_over = False
+        self.input_line = ""
+        self.input_helper = ""
+        self.player_inventory = [""]
+        print("Welcome to Dork")
 
     def check_game_over(self):
         """
         Check if the game is over
         """
-        return self.GAME_OVER
+        return self.game_over
 
     def set_game_over(self, cond):
         """
         Method for conditional termination of the game
         """
         if cond is True:
-            self.GAME_OVER = True
+            self.game_over = True
         if cond is not True:
-            self.GAME_OVER = False
+            self.game_over = False
 
     def read_command(self):
         """
@@ -53,44 +48,44 @@ class CommandManager:
         feed = ['feed']
         inventory = ['inventory']
 
-        self.INPUT_LINE = input("")
-        if self.INPUT_LINE in north:
-            self.INPUT_LINE = "go north"
-        elif self.INPUT_LINE in south:
-            self.INPUT_LINE = "go south"
-        elif self.INPUT_LINE in east:
-            self.INPUT_LINE = "go east"
-        elif self.INPUT_LINE in west:
-            self.INPUT_LINE = "go west"
-        elif (self.INPUT_LINE[0:2] in move or
-              self.INPUT_LINE[0:3] in move) and (len(self.INPUT_LINE) < 6):
-            self.INPUT_LINE = "go"
-        elif (self.INPUT_LINE[0:4] in move or
-              self.INPUT_LINE[0:5] in move) and (len(self.INPUT_LINE) < 6):
-            self.INPUT_LINE = "go"
-        elif self.INPUT_LINE[0:3] in put:
-            self.INPUT_HELPER = self.INPUT_LINE[4:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "put"
-        elif self.INPUT_LINE[0:3] in get:
-            self.INPUT_HELPER = self.INPUT_LINE[4:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "get"
-        elif self.INPUT_LINE[0:4] in get:
-            self.INPUT_HELPER = self.INPUT_LINE[5:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "get"
-        elif self.INPUT_LINE[0:3] in look:
-            self.INPUT_HELPER = self.INPUT_LINE[4:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "look"
-        elif self.INPUT_LINE[0:4] in look:
-            self.INPUT_HELPER = self.INPUT_LINE[5:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "look"
-        elif self.INPUT_LINE[0:3] in eat:
-            self.INPUT_HELPER = self.INPUT_LINE[4:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "eat"
-        elif self.INPUT_LINE[0:4] in feed:
-            self.INPUT_HELPER = self.INPUT_LINE[5:len(self.INPUT_LINE)]
-            self.INPUT_LINE = "feed"
-        elif self.INPUT_LINE in inventory:
-            self.INPUT_LINE = "inventory"
+        self.input_line = input("")
+        if self.input_line in north:
+            self.input_line = "go north"
+        elif self.input_line in south:
+            self.input_line = "go south"
+        elif self.input_line in east:
+            self.input_line = "go east"
+        elif self.input_line in west:
+            self.input_line = "go west"
+        elif (self.input_line[0:2] in move or
+              self.input_line[0:3] in move) and (len(self.input_line) < 6):
+            self.input_line = "go"
+        elif (self.input_line[0:4] in move or
+              self.input_line[0:5] in move) and (len(self.input_line) < 6):
+            self.input_line = "go"
+        elif self.input_line[0:3] in put:
+            self.input_helper = self.input_line[4:len(self.input_line)]
+            self.input_line = "put"
+        elif self.input_line[0:3] in get:
+            self.input_helper = self.input_line[4:len(self.input_line)]
+            self.input_line = "get"
+        elif self.input_line[0:4] in get:
+            self.input_helper = self.input_line[5:len(self.input_line)]
+            self.input_line = "get"
+        elif self.input_line[0:3] in look:
+            self.input_helper = self.input_line[4:len(self.input_line)]
+            self.input_line = "look"
+        elif self.input_line[0:4] in look:
+            self.input_helper = self.input_line[5:len(self.input_line)]
+            self.input_line = "look"
+        elif self.input_line[0:3] in eat:
+            self.input_helper = self.input_line[4:len(self.input_line)]
+            self.input_line = "eat"
+        elif self.input_line[0:4] in feed:
+            self.input_helper = self.input_line[5:len(self.input_line)]
+            self.input_line = "feed"
+        elif self.input_line in inventory:
+            self.input_line = "inventory"
 
     def execute_command(self):
         """
@@ -98,35 +93,35 @@ class CommandManager:
         """
         room_one_prints = Room1Printing()
         #print(inputLine[0:2] + "   " + inputLine[3:8]) #Debugging
-        if self.INPUT_LINE == "exit":
+        if self.input_line == "exit":
             print("Bye!")
             self.set_game_over(True)
-        elif self.INPUT_LINE[0:8] == "go north":
+        elif self.input_line[0:8] == "go north":
             room_one_prints.print_move("room 1", "north")
-        elif self.INPUT_LINE[0:8] == "go south":
+        elif self.input_line[0:8] == "go south":
             room_one_prints.print_move("room 1", "south")
-        elif self.INPUT_LINE[0:7] == "go east":
+        elif self.input_line[0:7] == "go east":
             room_one_prints.print_move("room 1", "east")
-        elif self.INPUT_LINE[0:7] == "go west":
+        elif self.input_line[0:7] == "go west":
             room_one_prints.print_move("room 1", "west")
-        elif self.INPUT_LINE == "go":
+        elif self.input_line == "go":
             print("Go where?")
-        elif self.INPUT_LINE == "look":
-            room_one_prints.print_look("room 1", self.INPUT_HELPER)
+        elif self.input_line == "look":
+            room_one_prints.print_look("room 1", self.input_helper)
             direction = ['north', 'south', 'east', 'west']
-            if self.INPUT_HELPER not in direction:
-                room_one_prints.print_examine(self.INPUT_HELPER)
-        elif self.INPUT_LINE == "get":
-            room_one_prints.print_get(self.INPUT_HELPER)
-            self.PLAYER_INVENTORY.append(self.INPUT_HELPER)
-        elif self.INPUT_LINE == "put":
+            if self.input_helper not in direction:
+                room_one_prints.print_examine(self.input_helper)
+        elif self.input_line == "get":
+            room_one_prints.print_get(self.input_helper)
+            self.player_inventory.append(self.input_helper)
+        elif self.input_line == "put":
             print("Put not ready")
-        elif self.INPUT_LINE == "eat":
-            room_one_prints.print_eat_food(self.INPUT_HELPER)
-        elif self.INPUT_LINE == "feed":
-            room_one_prints.print_feed_creature(self.INPUT_HELPER)
-        elif self.INPUT_LINE == "inventory":
-            room_one_prints.print_inventory(self.PLAYER_INVENTORY)
+        elif self.input_line == "eat":
+            room_one_prints.print_eat_food(self.input_helper)
+        elif self.input_line == "feed":
+            room_one_prints.print_feed_creature(self.input_helper)
+        elif self.input_line == "inventory":
+            room_one_prints.print_inventory(self.player_inventory)
             print("Inventory:")
         else:
             print("Invalid Command!")
